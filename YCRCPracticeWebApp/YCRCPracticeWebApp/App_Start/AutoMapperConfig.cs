1dac6c2428ebda214e3f0f906c1c12fe7a35027b
LiFang <LiFang@DESKTOP-H6859DL>
2018-05-01 17:33:13 +0800

    新增WebProfile 及 建立AutoMapperConfig 並註冊WebProfile及ServiceProfile

---
+++ YCRCPracticeWebApp/YCRCPracticeWebApp/App_Start/AutoMapperConfig.cs
@@ -0,0 +1,28 @@
+﻿using AutoMapper;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using YCRCPracticeWebApp.Mappings;
+using YCRCPracticeWebApp.Service.Mapping;
+
+namespace YCRCPracticeWebApp.App_Start
+{
+    /// <summary>
+    /// Class AutoMapperConfig.
+    /// </summary>
+    public static class AutoMapperConfig
+    {
+        /// <summary>
+        /// Registers the profile.
+        /// </summary>
+        public static void RegisterProfile()
+        {
+            Mapper.Initialize(config =>
+            {
+                config.AddProfile<WebProfile>();
+                config.AddProfile<ServiceProfile>();
+            });
+        }
+    }
+}
---
+++ YCRCPracticeWebApp/YCRCPracticeWebApp/Mappings/WebProfile.cs
@@ -0,0 +1,29 @@
+﻿using AutoMapper;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using YCRCPracticeWebApp.Models.ViewModels;
+using YCRCPracticeWebApp.Service.DataTransferObject;
+
+namespace YCRCPracticeWebApp.Mappings
+{
+    public class WebProfile:Profile
+    {
+        public WebProfile()
+        {
+            ViewModelToDto();
+            DtoToViewModel();
+        }
+
+        private void ViewModelToDto()
+        {
+            CreateMap<OrderViewModel, OrderDto>();
+        }
+
+        private void DtoToViewModel()
+        {
+            CreateMap<OrderDto, OrderViewModel>();
+        }
+    }
+}
