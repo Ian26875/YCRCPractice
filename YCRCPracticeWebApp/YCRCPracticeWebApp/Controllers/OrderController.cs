7819de742a15e6d066a83a308631cc1441e244f2
LiFang <LiFang@DESKTOP-H6859DL>
2018-05-01 16:57:50 +0800

    新增OrderController的List 及 OrderViewModel

--- YCRCPracticeWebApp/YCRCPracticeWebApp/Controllers/OrderController.cs
+++ YCRCPracticeWebApp/YCRCPracticeWebApp/Controllers/OrderController.cs
@@ -3,14 +3,26 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using YCRCPracticeWebApp.Models.ViewModels;
 
 namespace YCRCPracticeWebApp.Controllers
 {
+    /// <summary>
+    /// Class OrderController.
+    /// </summary>
+    /// <seealso cref="System.Web.Mvc.Controller" />
     public class OrderController : Controller
     {
-        // GET: Order
+
+
+
         public ActionResult Index()
         {
+            return View();
+        }
+
+        public ActionResult List()
+        {           
             return View();
         }
     }
