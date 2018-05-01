8504cf89c18549baea814f69d7e6828125f5709b
LiFang <LiFang@DESKTOP-H6859DL>
2018-05-01 13:43:13 +0800

    新增 IUnitOfWork 及 實作 UnitOfWork

---
+++ YCRCPracticeWebApp/YCRCPracticeWebApp.Repository/Interface/IUnitOfWork.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Data.Entity;
+
+namespace YCRCPracticeWebApp.Repository.Interface
+{
+    /// <summary>
+    /// Interface IUnitOfWork
+    /// </summary>
+    /// <seealso cref="System.IDisposable" />
+    public interface IUnitOfWork : IDisposable
+    {
+        /// <summary>
+        /// Gets or sets the data base context.
+        /// </summary>
+        /// <value>The data base context.</value>
+        DbContext DataBaseContext { get; set; }
+
+        /// <summary>
+        /// Saves the changes.
+        /// </summary>
+        /// <returns>System.Int32.</returns>
+        int SaveChanges();
+    }
+}
