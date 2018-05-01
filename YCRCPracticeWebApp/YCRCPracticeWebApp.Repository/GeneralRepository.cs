90abb220d39d5f10888848d8675b5d5d4c49d36b
LiFang <LiFang@DESKTOP-H6859DL>
2018-05-01 14:04:13 +0800

    新增IGeneralRepository 介面 及 GenaralRepository

---
+++ YCRCPracticeWebApp/YCRCPracticeWebApp.Repository/GeneralRepository.cs
@@ -0,0 +1,119 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Text;
+using System.Threading.Tasks;
+using YCRCPracticeWebApp.Repository.Interface;
+
+namespace YCRCPracticeWebApp.Repository
+{
+    /// <summary>
+    /// Class GeneralRepository.
+    /// </summary>
+    /// <typeparam name="TEntity">The type of the t entity.</typeparam>
+    public class GeneralRepository<TEntity> : IGeneralRepository<TEntity>
+        where TEntity : class, new()
+    {
+
+        /// <summary>
+        /// The unit of work
+        /// </summary>
+        private readonly IUnitOfWork _unitOfWork;
+
+        /// <summary>
+        /// The database set
+        /// </summary>
+        private readonly DbSet<TEntity> _dbSet;
+
+        /// <summary>
+        /// Initializes a new instance of the <see cref="GeneralRepository{TEntity}"/> class.
+        /// </summary>
+        /// <param name="unitOfWork">The unit of work.</param>
+        public GeneralRepository(IUnitOfWork unitOfWork)
+        {
+            this._unitOfWork = unitOfWork;
+            this._dbSet = unitOfWork.DataBaseContext.Set<TEntity>();
+        }
+
+        /// <summary>
+        /// Inserts the specified model.
+        /// </summary>
+        /// <param name="model">The model.</param>
+        public void Insert(TEntity model)
+        {
+            _dbSet.Add(model);
+        }
+
+        /// <summary>
+        /// Updates the specified model.
+        /// </summary>
+        /// <param name="model">The model.</param>
+        public void Update(TEntity model)
+        {
+            var entry = _unitOfWork.DataBaseContext.Entry(model);
+            entry.State = EntityState.Modified;
+        }
+
+        /// <summary>
+        /// Deletes the specified model.
+        /// </summary>
+        /// <param name="model">The model.</param>
+        public void Delete(TEntity model)
+        {
+            _dbSet.Remove(model);
+        }
+
+        /// <summary>
+        /// Wheres the specified predicate.
+        /// </summary>
+        /// <param name="predicate">The predicate.</param>
+        /// <returns>IQueryable&lt;TEntity&gt;.</returns>
+        public IQueryable<TEntity> Where(Expression<Func<TEntity, bool>> predicate)
+        {
+            return _dbSet.Where(predicate);
+        }
+
+        /// <summary>
+        /// Gets all.
+        /// </summary>
+        /// <returns>IQueryable&lt;TEntity&gt;.</returns>
+        public IQueryable<TEntity> GetAll()
+        {
+            return _dbSet;
+        }
+
+        /// <summary>
+        /// Firsts the or default.
+        /// </summary>
+        /// <param name="predicate">The predicate.</param>
+        /// <returns>TEntity.</returns>
+        public TEntity FirstOrDefault(Expression<Func<TEntity, bool>> predicate)
+        {
+            return _dbSet.FirstOrDefault(predicate);
+        }
+
+        /// <summary>
+        /// Singles the or default.
+        /// </summary>
+        /// <param name="predicate">The predicate.</param>
+        /// <returns>TEntity.</returns>
+        public TEntity SingleOrDefault(Expression<Func<TEntity, bool>> predicate)
+        {
+            return _dbSet.SingleOrDefault(predicate);
+        }
+
+        /// <summary>
+        /// Finds the specified key values.
+        /// </summary>
+        /// <param name="keyValues">The key values.</param>
+        /// <returns>TEntity.</returns>
+        public TEntity Find(params object[] keyValues)
+        {
+            return _dbSet.Find(keyValues);
+        }
+
+       
+    }
+}
---
+++ YCRCPracticeWebApp/YCRCPracticeWebApp.Repository/Interface/IGeneralRepository.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Linq;
+using System.Linq.Expressions;
+
+namespace YCRCPracticeWebApp.Repository.Interface
+{
+    /// <summary>
+    /// Interface IGeneralRepository
+    /// </summary>
+    /// <typeparam name="TEntity">The type of the t entity.</typeparam>
+    public interface IGeneralRepository<TEntity> where TEntity : class, new()
+    {
+        /// <summary>
+        /// Deletes the specified model.
+        /// </summary>
+        /// <param name="model">The model.</param>
+        void Delete(TEntity model);
+
+        /// <summary>
+        /// Finds the specified key values.
+        /// </summary>
+        /// <param name="keyValues">The key values.</param>
+        /// <returns>TEntity.</returns>
+        TEntity Find(params object[] keyValues);
+
+        /// <summary>
+        /// Firsts the or default.
+        /// </summary>
+        /// <param name="predicate">The predicate.</param>
+        /// <returns>TEntity.</returns>
+        TEntity FirstOrDefault(Expression<Func<TEntity, bool>> predicate);
+
+        /// <summary>
+        /// Gets all.
+        /// </summary>
+        /// <returns>IQueryable&lt;TEntity&gt;.</returns>
+        IQueryable<TEntity> GetAll();
+
+        /// <summary>
+        /// Inserts the specified model.
+        /// </summary>
+        /// <param name="model">The model.</param>
+        void Insert(TEntity model);
+
+        /// <summary>
+        /// Singles the or default.
+        /// </summary>
+        /// <param name="predicate">The predicate.</param>
+        /// <returns>TEntity.</returns>
+        TEntity SingleOrDefault(Expression<Func<TEntity, bool>> predicate);
+
+        /// <summary>
+        /// Updates the specified model.
+        /// </summary>
+        /// <param name="model">The model.</param>
+        void Update(TEntity model);
+
+        /// <summary>
+        /// Wheres the specified predicate.
+        /// </summary>
+        /// <param name="predicate">The predicate.</param>
+        /// <returns>IQueryable&lt;TEntity&gt;.</returns>
+        IQueryable<TEntity> Where(Expression<Func<TEntity, bool>> predicate);
+    }
+}
