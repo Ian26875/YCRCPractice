d516a6ddc628de9f1fae0e9a52727c7568887637
ianlchen <ianlchen@LTC-04406-0030>
2018-05-04 11:39:40 +0800

    撰寫GetPageOrders 方法測試案例,並加入Order_TestData.csv測試資料

--- YCRCPracticeWebApp/YCRCPracticeWebApp.ServiceTest/OrderServiceTest.cs
+++ YCRCPracticeWebApp/YCRCPracticeWebApp.ServiceTest/OrderServiceTest.cs
@@ -1,14 +1,98 @@
 ﻿using System;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
+using YCRCPracticeWebApp.Repository;
+using YCRCPracticeWebApp.Repository.Interface;
+using NSubstitute;
+using FluentAssertions;
+using YCRCPracticeWebApp.Service;
+using YCRCPracticeWebApp.Service.Interface;
+using AutoMapper;
+using YCRCPracticeWebApp.Service.Mapping;
 
 namespace YCRCPracticeWebApp.ServiceTest
 {
     [TestClass]
+    [DeploymentItem(@"TestData\Order_TestData.csv")]
     public class OrderServiceTest
     {
-        [TestMethod]
-        public void TestMethod1()
+
+        private IGeneralRepository<Orders> OrdeRepository { get; set; }
+
+        [TestInitialize]
+        public void TestInitialize()
         {
+            OrdeRepository = Substitute.For<IGeneralRepository<Orders>>();
+
+            //Resets the mapper configuration. Not intended for production use, but for testing scenarios.
+            Mapper.Reset();
+
+            Mapper.Initialize(config =>
+            {
+                config.AddProfile<ServiceProfile>();
+            });
+        }
+
+        /// <summary>
+        /// Gets the system under test.
+        /// </summary>
+        /// <returns>IOrderService.</returns>
+        private IOrderService GetSystemUnderTest()
+        {
+            var sut = new OrderService(OrdeRepository);
+            return sut;
+        }
+
+
+        [TestCategory("OrderService")]
+        [TestProperty("OrderService", "GetPageOrders")]
+        [TestMethod]
+        public void GetPageOrders_輸入pageNumber為0及pageSize為20_應ArgumentOutOfRangeException()
+        {
+            //arrange
+            var pageNumber = 0;
+            var pageSize = 20;
+            var sut = this.GetSystemUnderTest();         
+
+            //act
+            Action action = () => sut.GetPageOrders(pageNumber, pageSize);
+
+            //assert
+            action.Should().Throw<ArgumentOutOfRangeException>();
+        }
+
+
+        [TestCategory("OrderService")]
+        [TestProperty("OrderService", "GetPageOrders")]
+        [TestMethod]
+        public void GetPageOrders_輸入pageNumber為1及pageSize為0_應ArgumentOutOfRangeException()
+        {
+            //arrange
+            int pageNumber = 1;
+            int pageSize = 0;
+            var sut = this.GetSystemUnderTest();
+
+            //act
+            Action action = () => sut.GetPageOrders(pageNumber, pageSize);
+
+            //assert
+            action.Should().Throw<ArgumentOutOfRangeException>();
+        }
+
+        [TestCategory("OrderService")]
+        [TestProperty("OrderService", "GetPageOrders")]
+        [TestMethod]
+        public void GetPageOrders_輸入pageNumber為1及pageSize為20_應ArgumentOutOfRangeException()
+        {
+            //arrange
+            int pageNumber = 1;
+            int pageSize = 0;
+            var sut = this.GetSystemUnderTest();
+
+            //act
+            Action action = () => sut.GetPageOrders(pageNumber, pageSize);
+
+            //assert
+            action.Should().Throw<ArgumentOutOfRangeException>();
         }
     }
 }
