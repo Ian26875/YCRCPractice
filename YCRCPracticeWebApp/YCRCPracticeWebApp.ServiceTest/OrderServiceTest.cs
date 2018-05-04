944c397c0c3c10d7440e4aa268d2344e75beb03a
ianlchen <ianlchen@LTC-04406-0030>
2018-05-04 17:59:48 +0800

    新增 GetOrder 測試

--- YCRCPracticeWebApp/YCRCPracticeWebApp.ServiceTest/OrderServiceTest.cs
+++ YCRCPracticeWebApp/YCRCPracticeWebApp.ServiceTest/OrderServiceTest.cs
@@ -9,6 +9,7 @@ using FluentAssertions;
 using YCRCPracticeWebApp.Service;
 using YCRCPracticeWebApp.Service.Interface;
 using AutoMapper;
+using YCRCPracticeWebApp.Service.DataTransferObject;
 using YCRCPracticeWebApp.Service.Mapping;
 
 
@@ -103,7 +104,7 @@ namespace YCRCPracticeWebApp.ServiceTest
         [TestMethod]
         public void GetPageOrders_輸入pageNumber為1及pageSize為20_應取得20筆資料()
         {
-            
+
             //arrange
             int expected = 20;
             int pageNumber = 1;
@@ -119,10 +120,70 @@ namespace YCRCPracticeWebApp.ServiceTest
             var sut = this.GetSystemUnderTest();
 
             //act
-            var actual=sut.GetPageOrders(pageNumber, pageSize);
+            var actual = sut.GetPageOrders(pageNumber, pageSize);
 
             //assert
             actual.Should().HaveCount(expected);
+        }
+
+        [TestCategory("OrderService")]
+        [TestProperty("OrderService", "GetOrder")]
+        [TestMethod]
+        public void GetOrder_輸入orderId為負一_應ThrowArgumentOutOfRangeException()
+        {
+
+            //arrange         
+            int orderId = -1;
+
+            var sut = this.GetSystemUnderTest();
+
+            //act
+            Action action = () => sut.GetOrder(orderId);
+
+            //assert
+            action.Should().Throw<ArgumentOutOfRangeException>();
+        }
+
+
+        [TestCategory("OrderService")]
+        [TestProperty("OrderService", "GetOrder")]
+        [TestMethod]
+        public void GetOrder_輸入orderId_應取得該筆訂單資料()
+        {
+
+            //arrange         
+
+            var sut = this.GetSystemUnderTest();
+            Fixture fixture = new Fixture();
+            var source = fixture.Build<Orders>()
+                                .OmitAutoProperties()
+                                .Create();
+            var expected = new OrderDto
+            {
+                CustomerID = source.CustomerID,
+                EmployeeID = source.EmployeeID,
+                Freight = source.Freight,               
+                OrderID =source.OrderID,
+                OrderDate = source.OrderDate,
+                RequiredDate = source.RequiredDate,  
+                ShipAddress = source.ShipAddress,
+                ShipCity = source.ShipCity,
+                ShipCountry = source.ShipCountry,
+                ShippedDate = source.ShippedDate,
+                ShipName = source.ShipName,
+                ShipPostalCode = source.ShipPostalCode,
+                ShipRegion = source.ShipRegion,
+                ShipVia = source.ShipVia,
+            };
+
+            OrdeRepository.Find(null)
+                          .ReturnsForAnyArgs(source);
+            //act
+            var actual = sut.GetOrder(source.OrderID);
+
+            //assert
+          
+            actual.Should().BeEquivalentTo(expected);
         }
     }
 }
