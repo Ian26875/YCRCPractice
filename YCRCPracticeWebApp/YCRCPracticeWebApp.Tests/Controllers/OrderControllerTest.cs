5279d4bffbc97dfcffbfa8480fd022aee2b3ff8e
LiFang <LiFang@DESKTOP-H6859DL>
2018-05-07 21:37:00 +0800

    YCRCPracticeWebApp.Repository 更新EF WebTest 加入 NSubstitute , FluentAssertions ,AutoFixture

---
+++ YCRCPracticeWebApp/YCRCPracticeWebApp.Tests/Controllers/OrderControllerTest.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using YCRCPracticeWebApp.Service.Interface;
+
+namespace YCRCPracticeWebApp.Tests.Controllers
+{
+    /// <summary>
+    /// Class OrderControllerTest.
+    /// </summary>
+    [TestClass]
+    public class OrderControllerTest
+    {
+        private IOrderService OrderSvc { get; set; }
+
+        [TestInitialize]
+        public void TestInitialize()
+        {
+
+        }
+    }
+}
