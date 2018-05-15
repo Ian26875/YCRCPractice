87ec7667c75fd455be4ee9152790bb5050f2c2b2
ianlchen <ianlchen@LTC-04406-0030>
2018-05-15 09:59:06 +0800

    新增ProductServiceTest

---
+++ YCRCPracticeWebApp/YCRCPracticeWebApp.ServiceTest/ProductServiceTest.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using YCRCPracticeWebApp.Repository;
+using YCRCPracticeWebApp.Repository.Interface;
+using NSubstitute;
+using FluentAssertions;
+
+namespace YCRCPracticeWebApp.ServiceTest
+{
+    [TestClass]
+    public class ProductServiceTest
+    {
+        private IGeneralRepository<Products> ProductRepository { get; set; }
+
+        /// <summary>
+        /// Tests the initialize.
+        /// </summary>
+        [TestInitialize]
+        public void TestInitialize()
+        {
+            this.ProductRepository=Substitute.For<IGeneralRepository<Products>>();
+        }
+
+        
+
+    }
+}
