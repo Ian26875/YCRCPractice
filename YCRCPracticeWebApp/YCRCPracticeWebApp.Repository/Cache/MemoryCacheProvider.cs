dd07f8ed75b406aa06d1c842614c5f1fee84cb90
ianlchen <ianlchen@LTC-04406-0030>
2018-05-18 17:59:46 +0800

    新增 MemoryCacheProvider

---
+++ YCRCPracticeWebApp/YCRCPracticeWebApp.Repository/Cache/MemoryCacheProvider.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Caching;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace YCRCPracticeWebApp.Repository.Cache
+{
+    public class MemoryCacheProvider
+    {
+        /// <summary>
+        /// The memory cache
+        /// </summary>
+        private readonly MemoryCache _memoryCache;
+
+        /// <summary>
+        /// Initializes a new instance of the <see cref="MemoryCacheProvider"/> class.
+        /// </summary>
+        public MemoryCacheProvider()
+        {
+            this._memoryCache = MemoryCache.Default;
+        }
+
+        /// <summary>
+        /// Determines whether [contains] [the specified key].
+        /// </summary>
+        /// <param name="key">The key.</param>
+        /// <returns><c>true</c> if [contains] [the specified key]; otherwise, <c>false</c>.</returns>
+        public bool Contains(string key)
+        {
+            return _memoryCache.Contains(key);
+        }
+
+        /// <summary>
+        /// Gets the specified key.
+        /// </summary>
+        /// <typeparam name="TItem">The type of the t item.</typeparam>
+        /// <param name="key">The key.</param>
+        /// <returns>TItem.</returns>
+        public TItem Get<TItem>(string key)
+        {
+            return (TItem)_memoryCache.Get(key);
+        }
+
+        /// <summary>
+        /// Sets the specified key.
+        /// </summary>
+        /// <typeparam name="TItem">The type of the t item.</typeparam>
+        /// <param name="key">The key.</param>
+        /// <param name="item">The item.</param>
+        public void Set<TItem>(string key, TItem item)
+        {
+            var cacheItemPolicy = new CacheItemPolicy();
+            _memoryCache.Set(key, item, cacheItemPolicy);
+        }
+
+        /// <summary>
+        /// Removes the specified key.
+        /// </summary>
+        /// <param name="key">The key.</param>
+        public void Remove(string key)
+        {
+            _memoryCache.Remove(key);
+        }
+    }
+}
